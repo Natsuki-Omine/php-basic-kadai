565f21eec0ff0a310cf6dea36db5899682c6009b
Natsuki Omine <166575245+Natsuki-Omine@users.noreply.github.com>
2024-04-24 08:21:28 +0900

    PHP基礎kadai_007フォルダをアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎7章課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 連想配列を作成
+        $vegitable1 = ["neme" => "onion", "price" => 200, "weight" => 160];
+
+        // 連想配列$vegitable1を出力する
+        print_r($vegitable1);
+        ?>
+    </p>
+</body>
+
+</html>
